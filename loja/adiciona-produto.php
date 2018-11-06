1b32ac19b8b636c55021368cf6597a0c111b45a9
marcuspereiragithub <vinnycoast@gmail.com>
2018-11-06 14:21:52 -0300

    Creating class autoload and DAO - Data Access Object

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,9 +1,6 @@
 <?php 
 require_once("cabecalho.php");
-require_once("banco-produto.php");
 require_once("logica-usuario.php");
-require_once("class/Produto.php");
-require_once("class/Categoria.php");
 
 verificaUsuario();
 
@@ -21,8 +18,9 @@ if(array_key_exists('usado', $_POST)) {
 }
 
 $produto = new Produto($nome, $preco, $descricao, $categoria, $usado);
+$produtoDao = new ProdutoDao($conexao);
 
-if(insereProduto($conexao, $produto)) { ?>
+if($produtoDao->insereProduto($produto)) { ?>
 	<p class="text-success">O produto <?= $produto->getNome() ?>, <?= $produto->getPreco() ?> foi adicionado.</p>
 <?php 
 } else {
