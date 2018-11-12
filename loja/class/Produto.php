16ca4a09dc9db062604b43e412b354f4fc602041
marcuspereiragithub <vinnycoast@gmail.com>
2018-11-12 14:52:09 -0300

    Leaving the abstract class Produto because when a class has an abstract method, it must also be abstract

--- loja/class/Produto.php
+++ loja/class/Produto.php
@@ -1,6 +1,6 @@
 <?php
 
-class Produto {
+abstract class Produto {
 
 	private $id;
 	private $nome;
@@ -70,19 +70,7 @@ class Produto {
 		return $this instanceof Ebook;
 	}
 
-	public function atualizaBaseadoEm($params) {
-		if($this->temIsbn()) {
-			$this->setIsbn($params['isbn']);
-		}
-
-		if($this->temTaxaImpressao()) {
-			$this->setTaxaImpressao($params['taxaImpressao']);
-		}
-
-		if($this->temWaterMark()) {
-			$this->setWaterMark($params['waterMark']);
-		}
-	}
+	abstract function atualizaBaseadoEm($params);
 
 	public function calculaImposto() {
 		return $this->preco * 0.195;
