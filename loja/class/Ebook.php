878da11b8aea402e5fba393332a63c9b47589a14
marcuspereiragithub <vinnycoast@gmail.com>
2018-11-12 11:38:17 -0300

    class Ebook when a book has watermark

---
+++ loja/class/Ebook.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Ebook extends Livro {
+
+        private $waterMark;
+
+        public function getWaterMark() {
+            return $this->waterMark;
+        }
+
+        public function setWaterMark($waterMark) {
+            $this->waterMark = $waterMark;
+        }
+
+    }
+?>
